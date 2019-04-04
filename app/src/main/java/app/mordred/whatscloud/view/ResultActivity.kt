5caa85a2ce52d0b7e1aa5148dda11f2a6af0bf72
sirmordred <oguzhan97yigit@gmail.com>
2019-04-04 15:35:28 +0300

    Added fresh new app icon * Also removed some dead codes

--- app/src/main/java/app/mordred/whatscloud/view/ResultActivity.kt
+++ app/src/main/java/app/mordred/whatscloud/view/ResultActivity.kt
@@ -2,11 +2,9 @@ package app.mordred.whatscloud.view
 
 import android.content.Intent
 import android.content.res.Resources
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
